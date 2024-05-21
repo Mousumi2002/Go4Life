50b8e9adb5b239a3ec21b92e238d17d7dcb07249
mousumi <moumipal2002@gmail.com>
2024-05-22 01:06:52 +0530

    Modify order med page
    Signed-off-by: mousumi <moumipal2002@gmail.com>

--- lib/pages/order_med.dart
+++ lib/pages/order_med.dart
@@ -12,6 +12,7 @@ class OrderMedicinePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final medicines = vendor.medicines;
     final outOfStockMedicines = getOutOfStock(vendor.outOfStockMedicines);
+    final requiresPrescriptions = getPrescriptionsRequirement(vendor.medicines);
     return Scaffold(
       appBar: const AppBarTitle(),
       body: Column(
@@ -29,8 +30,7 @@ class OrderMedicinePage extends StatelessWidget {
               ),
               const Text(
                 'Order Medicine',
-                style: TextStyle(
-                    color: Color.fromARGB(255, 10, 55, 214), fontSize: 22),
+                style: TextStyle(color: Color.fromARGB(255, 10, 55, 214), fontSize: 22),
               ),
             ],
           ),
@@ -60,10 +60,7 @@ class OrderMedicinePage extends StatelessWidget {
                         children: [
                           Text(
                             vendor.name,
-                            style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black87),
+                            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.black87),
                           ),
                           const SizedBox(height: 8),
                           const Text(
@@ -83,10 +80,7 @@ class OrderMedicinePage extends StatelessWidget {
                     Expanded(
                       child: Text(
                         '${medicines[0]['currency']} ${getPrice(medicines)}',
-                        style: const TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black),
+                        style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w400, color: Colors.black),
                         maxLines: 2,
                       ),
                     ),
@@ -94,8 +88,7 @@ class OrderMedicinePage extends StatelessWidget {
                 ),
                 if (outOfStockMedicines.isNotEmpty)
                   Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 30, vertical: 4),
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 4),
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -117,48 +110,46 @@ class OrderMedicinePage extends StatelessWidget {
                       ],
                     ),
                   ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 35, vertical: 4),
-                  child: Row(
-                    children: [
-                      Image.asset('assets/prescription.png'),
-                      const SizedBox(width: 8),
-                      const Text(
-                        "Some medicines requires prescription",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400),
-                      ),
-                    ],
+                if (requiresPrescriptions) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 35,
+                      vertical: 4,
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/prescription.png'),
+                        const SizedBox(width: 8),
+                        const Text(
+                          "Some medicines requires prescription",
+                          style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      // ignore: sort_child_properties_last
-                      child: const Text('Upload Prescription'),
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color.fromARGB(255, 102, 156, 255)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        // ignore: sort_child_properties_last
+                        child: const Text('Upload Prescription'),
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 102, 156, 255)),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                ],
                 Padding(
-                  padding: const EdgeInsets.only(right: 150),
+                  padding: const EdgeInsets.only(right: 180),
                   child: Text(
                     "Available Items (${medicines.length})",
                     style: const TextStyle(
@@ -175,15 +166,13 @@ class OrderMedicinePage extends StatelessWidget {
                     height: 340,
                     width: 360,
                     decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 235, 242, 255),
-                        borderRadius: BorderRadius.circular(10.0)),
+                        color: const Color.fromARGB(255, 235, 242, 255), borderRadius: BorderRadius.circular(10.0)),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 14, vertical: 12),
+                          padding: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
                           child: Text(
                             "List of Medicine ordered",
                             style: TextStyle(
@@ -197,9 +186,9 @@ class OrderMedicinePage extends StatelessWidget {
                           children: [
                             Expanded(
                               child: ListView.builder(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 8, horizontal: 12),
+                                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
                                 itemBuilder: (context, index) {
+                                  final medicine = medicines[index];
                                   final item = medicines[index]['name'];
                                   return Row(
                                     children: [
@@ -207,12 +196,14 @@ class OrderMedicinePage extends StatelessWidget {
                                         item.toString(),
                                         style: const TextStyle(fontSize: 16),
                                       ),
+                                      if (medicine['prescription']) ...[
+                                        const SizedBox(width: 8),
+                                        Image.asset('assets/prescription.png'),
+                                      ],
                                       const Spacer(),
                                       Text(
-                                        'Units: ${medicines[index]['quantity']}',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: Colors.grey.shade700),
+                                        'Units: ${medicine['quantity']}',
+                                        style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
                                       )
                                     ],
                                   );
@@ -289,6 +280,7 @@ class OrderMedicinePage extends StatelessWidget {
     if (outOfStock.isEmpty) return '';
     return outOfStock;
   }
+
   showAlertDialog(BuildContext context) {
     // show the dialog
     showDialog(
@@ -308,5 +300,14 @@ class OrderMedicinePage extends StatelessWidget {
         );
       },
     );
+  }
+
+  bool getPrescriptionsRequirement(List medicines) {
+    for (var medicine in medicines) {
+      if (medicine['prescription']) {
+        return true;
+      }
+    }
+    return false;
   }
 }
